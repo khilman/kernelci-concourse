b47536ce3835bc033234ce1da65e2f275bdbba0f
Kevin Hilman <khilman@baylibre.com>
2022-08-31 07:56:52 -0700

    snapshot

--- tasks/build-trigger.sh
+++ tasks/build-trigger.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#set -x
 env
+set -x
 
-KCI_STORAGE_URL="${KCI_STORAGE_URL:-https://storage.staging.kernelci.org}"
-NEW_COMMITS=${PWD}/monitor-out/new-commits.txt
+KDIR=$PWD/kernel-repo
+echo "BUILD_CONFIG: $BUILD_CONFIG"
 
-if [[ ! -s ${NEW_COMMITS} ]]; then
-    echo "WARNING: No new commits found."
-    exit 1
+cd kernelci-core
+
+# config already built?
+NEW_COMMIT=$(./kci_build check_new_commit --build-config $BUILD_CONFIG --storage $KCI_STORAGE_URL)
+if [[ -z $NEW_COMMIT ]]; then
+    exit 0
 fi
 
-echo "New commits:"
-cat ${NEW_COMMITS}
+(cd $KDIR; ls -l; ls -l .git; git describe)
+ls -l .git
+./kci_build describe --build-config $BUILD_CONFIG --kdir $KDIR
